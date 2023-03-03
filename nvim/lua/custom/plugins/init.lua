c983472d278b621211b709ab3447b90484b01318
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-03-04 01:37:02 +1100

    Added lsp for nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,6 +2,42 @@ return {
   -- Enable dashboard
   ["goolord/alpha-nvim"] = {
     disable = false,
+  },
+  -- LSP server
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  -- mason.vim
+  -- install using :MasonInstallAll
+   ["williamboman/mason.nvim"] = {
+   override_options = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "emmet-ls",
+        "json-lsp",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+
+        -- custom
+        "autopep8",
+        "cfn-lint",
+        "terraform-ls",
+        "markdownlint"
+      }
+    }
   }
 }
 
