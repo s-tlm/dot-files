48a2a2a2799e3435d43f0f76699f6baa5bde4708
Steven Lam <stevelam@deloitte.com.au>
2023-03-06 19:18:10 +1100

    Set textwidth to 80.

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -6,4 +6,5 @@ opt.relativenumber = true
 
 -- Color lines
 opt.colorcolumn = "80"
+opt.textwidth = 80 -- automatically create new line when text width is hit
 cmd[[highlight ColorColumn ctermbg=0 guibg=lightgrey]]
