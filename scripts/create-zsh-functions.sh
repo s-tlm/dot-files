0c6fd8635d01b6864165ef7b4754b97827a0fae2
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-03-21 07:24:06 +1100

    Add script to symlink zsh functions

---
+++ scripts/create-zsh-functions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# SHARED DIRS
+REPO_DIR=$(git rev-parse --show-toplevel)
+ZSH_TGT_FUNC_DIR="$HOME/.zsh_functions"
+ZSH_SRC_FUNC_DIR="$REPO_DIR/zsh/functions"
+HELPERS_DIR="$REPO_DIR/scripts/utils"
+
+# Source helpers
+source "$HELPERS_DIR/helper_scripts.sh"
+
+printf "Configuring ZSH functions...\n"
+create_dir_ifn_exists "$ZSH_TGT_FUNC_DIR"
+
+# Create symlinks
+create_symlink "$ZSH_SRC_FUNC_DIR/mtp" "$ZSH_TGT_FUNC_DIR/mtp"
