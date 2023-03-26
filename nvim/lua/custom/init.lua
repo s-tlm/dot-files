13a69d25f863ff460218a984b98caa4fa5c4281b
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-03-27 08:39:31 +1100

    Add vim markdown settings

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -7,4 +7,8 @@ opt.relativenumber = true
 -- Color lines
 opt.colorcolumn = "80"
 opt.textwidth = 80 -- automatically create new line when text width is hit
-cmd[[highlight ColorColumn ctermbg=0 guibg=lightgrey]]
+cmd[[ highlight ColorColumn ctermbg=0 guibg=lightgrey ]]
+
+-- Vim Markdown settings
+cmd[[ let g:vim_markdown_folding_disabled=1 ]]
+cmd [[ set conceallevel=2 ]]
