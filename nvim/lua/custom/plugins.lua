2084e2a95158cf96aa67fe880d2310e44011409f
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-03-28 20:22:39 +1100

    Add neorg autocompletion module

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -120,6 +120,7 @@ local plugins = {
       load = {
         ["core.defaults"] = {}, -- Loads default behaviour
         ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents,
+        ["core.norg.completion"] = {},
         ["core.norg.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
