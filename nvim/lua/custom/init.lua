2c006588cf31de1622fc48f153a401799b53b27b
Steven Lam <stevelam@deloitte.com.au>
2023-04-27 17:48:15 +1000

    Remove auto new line.

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -8,7 +8,6 @@ opt.relativenumber = true
 
 -- Color lines
 opt.colorcolumn = "80"
-opt.textwidth = 80 -- automatically create new line when text width is hit
 cmd[[ highlight ColorColumn ctermbg=0 guibg=lightgrey ]]
 
 -- Neorg specific configs
