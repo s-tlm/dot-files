5544bffdd793bfb7c374da2e91be2dd97aa4e27e
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-06-03 11:53:49 +1000

    Accidental space

--- nvim/lunarvim/config.lua
+++ nvim/lunarvim/config.lua
@@ -32,7 +32,6 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
-
 -- always installed on startup, useful for parsers without a strict filetype
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
