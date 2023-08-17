c93a3a93fa20f9a6d0f0d9422d6115ad66c2c4e1
Steven Lam <stevelam@deloitte.com.au>
2023-08-17 12:32:24 +1000

    Add Scala LSP with minimal config.

--- nvim/lunarvim/config.lua
+++ nvim/lunarvim/config.lua
@@ -107,7 +107,65 @@ lvim.plugins = {
         }
       }
     },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "scalameta/nvim-metals",
+    ft = { "scala", "sbt" },
+    event = "BufEnter",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      local api = vim.api
+      local metals_config = require("metals").bare_config()
+      local dap = require("dap")
+
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      }
+
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
+
+      metals_config.on_attach = function(client, bufnr)
+        require("metals").setup_dap()
+      end
+
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
   },
   {
     "Pocco81/true-zen.nvim",
