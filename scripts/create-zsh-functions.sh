afc06027755f1f3d194fe6793c2fc0461358dfd7
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-11-18 09:32:56 +1100

    Remove unused function

--- scripts/create-zsh-functions.sh
+++ scripts/create-zsh-functions.sh
@@ -2,17 +2,12 @@
 
 # SHARED DIRS
 REPO_DIR=$(git rev-parse --show-toplevel)
+HELPERS_DIR="$REPO_DIR/scripts/utils"
 ZSH_TGT_FUNC_DIR="$HOME/.zsh_functions"
 ZSH_SRC_FUNC_DIR="$REPO_DIR/zsh/functions"
-HELPERS_DIR="$REPO_DIR/scripts/utils"
 
 # Source helpers
 source "$HELPERS_DIR/helper_scripts.sh"
 
 printf "Configuring ZSH functions...\n"
-create_dir_ifn_exists "$ZSH_TGT_FUNC_DIR"
-
-# Create symlinks by looping through each function
-for f in "${ZSH_SRC_FUNC_DIR}"/*; do
-  create_symlink "$f" "$ZSH_TGT_FUNC_DIR"/.
-done
+create_symlink "${ZSH_SRC_FUNC_DIR}" "${ZSH_TGT_FUNC_DIR}"
