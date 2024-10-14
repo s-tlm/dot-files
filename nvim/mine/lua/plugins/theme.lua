06de55e6461f125521f290ff9a53e2f296f20bc3
Steven Lam <slam@Stevens-MacBook-Air.local>
2024-10-14 22:30:22 +1100

    Add gitsigns

--- nvim/mine/lua/plugins/theme.lua
+++ nvim/mine/lua/plugins/theme.lua
@@ -12,17 +12,17 @@ return {
 				telescope = { enabled = true },
 				treesitter = true,
 				which_key = true,
-			}
+			},
 		},
 		config = function()
 			-- load the colourscheme
 			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
+	{ "RRethy/vim-illuminate" },
+	{ "HiPhish/rainbow-delimiters.nvim" },
 	{
-		"RRethy/vim-illuminate",
-	},
-	{
-		"HiPhish/rainbow-delimiters.nvim",
+		"lewis6991/gitsigns.nvim",
+		opts = {},
 	},
 }
