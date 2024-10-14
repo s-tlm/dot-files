58823a45a4d7caa2b0df40a6fb967ad8f3868e58
Steven Lam <slam@Stevens-MacBook-Air.local>
2024-10-14 21:58:38 +1100

    Add linter

--- nvim/mine/lua/plugins/lsp.lua
+++ nvim/mine/lua/plugins/lsp.lua
@@ -67,4 +67,32 @@ return {
 			})
 		end,
 	},
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				lua = { "selene" },
+				markdown = { "vale" },
+				python = { "ruff" },
+				sql = { "sqlfluff" },
+				terraform = { "tflint" },
+			}
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>l", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
+		end,
+	},
 }
--- nvim/mine/lua/plugins/which-key.lua
+++ nvim/mine/lua/plugins/which-key.lua
@@ -2,28 +2,28 @@ local builtin = require("telescope.builtin")
 
 return {
 	{
-	  "folke/which-key.nvim",
-	  event = "VeryLazy",
-	  opts = {
-		  spec = {
-			  { "<leader>pi", ":Lazy<CR>", { desc = "Show Lazy UI" } },
-			  { "<leader>e", ":NvimTreeToggle<CR>", {desc = "Toggle navigation tree"} },
-			  { "<leader>sf", builtin.find_files, { desc = "Telescope search files" } },
-		      { "<leader>st", builtin.live_grep, { desc = "Telescope search text" } },
-	          { "<leader>sb", builtin.buffers, { desc = "Telescope search buffers" } },
-	          { "<leader>sh", builtin.help_tags, { desc = "Telescope search help tags" } },
-	          { "<leader>li", ":Mason<CR>", { desc = "Launch Mason GUI" } },
-	          { "<leader>lu", ":MasonUpdate<CR>", { desc = "Update all Mason managed registries" } },
-		  },
-	  },
-	  keys = {
-		{
-		  "<leader>?",
-		  function()
-			require("which-key").show({ global = false })
-		  end,
-		  desc = "Buffer Local Keymaps (which-key)",
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			spec = {
+				{ "<leader>pi", ":Lazy<CR>", { desc = "Show Lazy UI" } },
+				{ "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle navigation tree" } },
+				{ "<leader>sf", builtin.find_files, { desc = "Telescope search files" } },
+				{ "<leader>st", builtin.live_grep, { desc = "Telescope search text" } },
+				{ "<leader>sb", builtin.buffers, { desc = "Telescope search buffers" } },
+				{ "<leader>sh", builtin.help_tags, { desc = "Telescope search help tags" } },
+				{ "<leader>Li", ":Mason<CR>", { desc = "Launch Mason GUI" } },
+				{ "<leader>Lu", ":MasonUpdate<CR>", { desc = "Update all Mason managed registries" } },
+			},
 		},
-	  },
-	}
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
