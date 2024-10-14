d9e50c5cc405b9e5954d1fbe29b4e46f361926e1
Steven Lam <slam@Stevens-MacBook-Air.local>
2024-10-15 07:55:48 +1100

    Rename and create lsp config file

--- nvim/mine/init.lua
+++ nvim/mine/init.lua
@@ -1,11 +1,1 @@
 require("config")
-
-vim.opt.relativenumber = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-
---
+++ nvim/mine/lua/config/general.lua
@@ -0,0 +1,8 @@
+vim.opt.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
