6e55b813ee2fdc2a63b84c92b7de8fb8919c801b
Steven Lam <stevelam@deloitte.com.au>
2024-10-15 19:51:14 +1100

    Fix treesitter lazy config

--- nvim/mine/lua/plugins/lsp.lua
+++ nvim/mine/lua/plugins/lsp.lua
@@ -20,23 +20,28 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		opts = {
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"json",
-				"jsonc",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"sql",
-				"terraform",
-				"yaml",
-			},
-		},
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"sql",
+					"terraform",
+					"hcl",
+					"yaml",
+				},
+			})
+		end,
 	},
 	{
 		"kylechui/nvim-surround",
