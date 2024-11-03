70f6c464e0e66fac943e25763c14d411e397df2f
Steven Lam <slam@Stevens-MacBook-Air.local>
2024-11-03 19:26:14 +1100

    Reformat lua

--- nvim/mine/lua/plugins/navigation.lua
+++ nvim/mine/lua/plugins/navigation.lua
@@ -1,22 +1,22 @@
 return {
 	{
-		  "nvim-tree/nvim-tree.lua",
-		  version = "*",
-		  lazy = false,
-		  dependencies = {
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
 			"nvim-tree/nvim-web-devicons",
-		  },
-		  config = function()
-			require("nvim-tree").setup {}
-		  end,
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
 	},
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = '0.1.8',
+		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make"
+		build = "make",
 	},
 }
