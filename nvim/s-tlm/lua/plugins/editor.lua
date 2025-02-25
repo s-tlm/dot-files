92e834dd1eafa91e9553773207a056e3664c3208
Steven Lam <slam@Stevens-MacBook-Air.local>
2025-02-25 18:27:15 +1100

    Move surround.nvim to editor.lua

--- nvim/s-tlm/lua/plugins/editor.lua
+++ nvim/s-tlm/lua/plugins/editor.lua
@@ -3,4 +3,10 @@ return {
 		"numToStr/Comment.nvim",
 		opts = {},
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		opts = {},
+	},
 }
