c9c9a2ea726a7e29acdd1aedd2fe199fd5e5f231
Steven Lam <slam@Stevens-MacBook-Air.local>
2025-02-26 08:52:18 +1100

    Reshuffle keybindings

---
+++ nvim/s-tlm/lua/config/keybinds.lua
@@ -0,0 +1,27 @@
+local wk = require("which-key")
+local builtin = require("telescope.builtin")
+
+wk.add({
+	{ "<C-S-l>", ":bnext<CR>", desc = "Next buffer" },
+	{ "<C-S-h>", ":bprevious<CR>", desc = "Previous buffer" },
+	{ "<leader>pi", ":Lazy<CR>", desc = "Show Lazy UI" },
+	{ "<leader>pu", ":Lazy update<CR>", desc = "Update Lazy plugins" },
+	{ "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle navigation tree" },
+	{ "<leader>sf", builtin.find_files, { desc = "Telescope search files" } },
+	{ "<leader>st", builtin.live_grep, { desc = "Telescope search text" } },
+	{ "<leader>sb", builtin.buffers, { desc = "Telescope search buffers" } },
+	{ "<leader>sh", builtin.help_tags, { desc = "Telescope search help tags" } },
+	{ "<leader>Li", ":Mason<CR>", desc = "Launch Mason GUI" },
+	{ "<leader>Lu", ":MasonUpdate<CR>", desc = "Update all Mason managed registries" },
+	{ "<leader>sb", desc = "Search buffer" },
+	{ "<leader>sh", desc = "Search help" },
+	{ "<leader>sf", desc = "Search file" },
+	{ "<leader>st", desc = "Search text" },
+	{
+		"<leader>ds",
+		function()
+			vim.diagnostic.open_float()
+		end,
+		desc = "Show diagnostics",
+	},
+})
---
+++ nvim/s-tlm/lua/plugins/which-key.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			spec = {
+				{ "<leader>p", group = "Lazy" },
+				{ "<leader>L", group = "Mason" },
+				{ "<leader>l", group = "Conform" },
+				{ "<leader>s", group = "Fuzzy Search" },
+				{ "<leader>d", group = "Diagnostics" },
+			},
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+}
