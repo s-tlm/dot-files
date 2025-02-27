d5e61b6ff00b9c6562eaa7f87e053d028379249f
Steven Lam <slam@Stevens-MacBook-Air.local>
2025-02-27 20:47:51 +1100

    Add some additional config to Alpha

--- nvim/s-tlm/lua/config/alpha.lua
+++ nvim/s-tlm/lua/config/alpha.lua
@@ -1,6 +1,7 @@
 local alpha = require("alpha")
 local builtin = require("telescope.builtin")
 local dashboard = require("alpha.themes.dashboard")
+local lazy = require("lazy")
 
 dashboard.section.header.val = {
 	"                                                    ",
@@ -15,15 +16,25 @@ dashboard.section.header.val = {
 
 -- set dashboard buttons
 dashboard.section.buttons.val = {
-	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("<leader>sf", "  > Find files", builtin.find_files),
+	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("o", "󰌑  > Open recent", ":Telescope oldfiles<CR>"),
+	dashboard.button("<leader>sf", "  > Search files", builtin.find_files),
+	dashboard.button("<leader>st", "󱩾  > Search text", builtin.live_grep),
+	dashboard.button("<leader>sb", "  > Search buffers", builtin.buffers),
+	dashboard.button("<leader>sb", "󰞋  > Search help", builtin.help_tags),
 	dashboard.button("q", "󰩈  > Quit NVIM", ":qa<CR>"),
 }
 
--- dashboard.section.buttons.val = {
--- 	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
--- 	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
--- }
+local function footer()
+	local total_plugins = lazy.stats().count
+	local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+	local version = vim.version()
+	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+	return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+end
+
+dashboard.section.footer.val = footer()
 
 -- send config to alpha
 alpha.setup(dashboard.opts)
