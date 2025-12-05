896749d2fbcd32efab54cbd72da936b1b6a5f27b
Steven Lam <steventienlam@gmail.com>
2025-12-05 23:44:24 +1100

    (blink.cmp) remove keybinds already defined in default preset

--- nvim/s-tlm/lua/plugins/lsp.lua
+++ nvim/s-tlm/lua/plugins/lsp.lua
@@ -146,13 +146,9 @@ return {
 		opts = {
 			keymap = {
 				preset = "default",
-				["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-e>"] = { "cancel" },
 				["<CR>"] = { "accept", "fallback" },
 				["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
 				["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f>"] = { "scroll_documentation_down", "fallback" },
 			},
 			appearance = {
 				nerd_font_variant = "mono",
