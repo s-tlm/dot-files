e6f6b2c0f77489c60673e90c4219396249a87ee4
Steven Lam <steventienlam@gmail.com>
2025-12-16 21:27:38 +1100

    add ghostty to symlink

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -18,30 +18,15 @@ NV_TGT_DIR="$CONFIG_DIR/nvim"
 SS_SRC_DIR="$REPO_DIR/starship/starship.toml"
 SS_TGT_TOML="$CONFIG_DIR/starship.toml"
 
+# GHOSTTY
+GHOSTTY_SRC_DIR="$REPO_DIR/ghostty"
+GHOSTTY_TGT_DIR="$CONFIG_DIR/ghostty"
+
 # Helper scripts
 source "$REPO_DIR/scripts/utils/helper_scripts.sh"
 
-# Main
-echo ""
-echo "Are the following installed?"
-echo "----------------------------"
-echo "SHELL - zsh"
-echo "SHELL - oh-my-zsh"
-echo "SHELL - starship"
-echo "IDE   - neovim"
-echo "----------------------------"
-read -r -p "Continue with setup? [yn] " input
-echo "----------------------------"
-
-if [[ $input =~ ^[Yy]$ ]]; then
-
-    create_symlink "$ZSH_SRC_DIR/.zshrc" "$HOME/.zshrc"
-    create_symlink "$ZSH_SRC_DIR/.zshenv" "$HOME/.zshenv"
-    create_symlink "$NV_SRC_DIR" "$NV_TGT_DIR"
-    create_symlink "$SS_SRC_DIR" "$SS_TGT_TOML"
-
-    echo ""
-    echo "Done."
-
-fi
-
+create_symlink "$ZSH_SRC_DIR/.zshrc" "$HOME/.zshrc"
+create_symlink "$ZSH_SRC_DIR/.zshenv" "$HOME/.zshenv"
+create_symlink "$NV_SRC_DIR" "$NV_TGT_DIR"
+create_symlink "$SS_SRC_DIR" "$SS_TGT_TOML"
+create_symlink "$GHOSTTY_SRC_DIR" "$GHOSTTY_TGT_DIR"
